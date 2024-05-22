2b71f83b8a1ab6dbdc557c8fbc4c0c57ab6b5acc
darneees <dgs56301@gmail.com>
2024-05-22 20:16:55 -0300

    style(home-page): adding new function so dark theme remains saved in local storage

--- resource/js/app.js
+++ resource/js/app.js
@@ -18,11 +18,29 @@ function toggleTheme() {
     if (element.classList.contains('darkMode')) {
         themeIcon.classList.remove('iconoir-half-moon');
         themeIcon.classList.add('iconoir-sun-light');
+
+        localStorage.setItem('theme', 'dark');
     } else {
         themeIcon.classList.remove('iconoir-sun-light');
         themeIcon.classList.add('iconoir-half-moon');
+
+        localStorage.setItem('theme', 'light');
     }
 }
+
+// saved theme function
+
+function apllySavedTheme() {
+    const savedTheme = localStorage.getItem('theme');
+
+    if(savedTheme) {
+        document.body.classList.add(savedTheme === 'dark' ? 'darkMode' : 'lightMode');
+    } else {
+        document.body.classList.add('lightMode');
+    }
+}
+
+apllySavedTheme();
 
 // open and close modal function
 
